18f99bed706db3ec2260afe7394307b3b8487cab
b-rand <brandonbatesit@outlook.com>
2023-11-03 00:01:43 -0400

    way back

---
+++ scripts/compendium.js
@@ -0,0 +1,8 @@
+Hooks.once("ready", function () {
+    game.packs.register("DnD5e-Compendia.backgrounds", {
+        label: "Backgrounds",
+        entity: "Item",
+        documents: [],
+        system: "dnd5e",
+    });
+});
