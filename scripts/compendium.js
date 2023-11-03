66fc53247d46305ee2319c086641504e6ed49089
b-rand <brandonbatesit@outlook.com>
2023-11-03 00:50:57 -0400

    Last Attempt

--- scripts/compendium.js
+++
@@ -1,8 +0,0 @@
-Hooks.once("ready", function () {
-    game.packs.register("dnd5e-complete-compendia.backgrounds", {
-        label: "Backgrounds",
-        entity: "Item",
-        documents: [],
-        system: "dnd5e",
-    });
-});
