242d0235b289e8cf549b0f83705b3b1df1ab6d6b
b-rand <brandonbatesit@outlook.com>
2023-11-02 22:46:59 -0400

    copy-items

---
+++ scripts/copy-items.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+
+async function addItemsToCompendium(itemDataArray) {
+    const compendiumName = "dnd5e-complete-compendia.items";
+    const compendium = game.packs.get(compendiumName);
+
+    if (compendium) {
+        for (const itemData of itemDataArray) {
+            // Create a new item and set its data
+            const item = await Item.create(itemData);
+            // Import the item into the compendium
+            await compendium.importEntity(item);
+            console.log(`Item "${itemData.name}" added to the compendium.`);
+        }
+    } else {
+        console.error(`Compendium "${compendiumName}" not found.`);
+    }
+}
+
+// Read the JSON file and parse its content
+const itemsFile = path.join('libraries', 'items.json');
+const rawItemsData = fs.readFileSync(itemsFile, 'utf-8');
+const itemsArray = JSON.parse(rawItemsData);
+
+addItemsToCompendium(itemsArray);
